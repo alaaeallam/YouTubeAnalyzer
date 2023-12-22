9172e46bba5ac4ff9dadf39a9904c3c735d95214
Alaa Allam <alaaeallam@gmail.com>
2023-12-22 22:03:55 +0200

    test2

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ youtube_url='https://www.youtube.com/watch?v=RObw_WRbX6k'
 
 Video_id=YouTube(youtube_url).video_id
 transcript= YouTubeTranscriptApi.get_transcript(Video_id)
+Video_view= YouTube(youtube_url).views
 text_transcript=""
 
 for segment in transcript:
     text_transcript += segment['text'] + " "
 print(text_transcript)
+print(Video_view)
