a496c93979a36858d934f0c7e00b0030512084b1
Alaa Allam <alaaeallam@Alaas-MacBook-Pro.local>
2023-12-22 21:52:36 +0200

    install

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print('Hello world')
+
+from pytube import YouTube
+
+youtube_url='https://www.youtube.com/watch?v=NHzu69AMVFw'
+
+yt=YouTube(youtube_url)
+
+if(yt):
+    print('Title:',yt.title)
+    print('Views:',yt.views)
+    print('Author:',yt.author)
+    print('Description:',yt.description)
